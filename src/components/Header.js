1df9a88d8531d13029b84b657b64132d042277ac
Samuel Olatunji <samuel.olatunji@coronationinsurance.com.ng>
2022-06-09 01:12:35 +0100

    Button and Event

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types' 
+import Button from './Button'
  const Header = ({title}) => {
+const onClick = () => {
+console.log('Clik')
+}
   return (
-    <header>
+    <header className='header'>
         <h1>{title}</h1>
-         
-    
+
+    <Button color='green' text='Add' onClick ={onClick} />
+
     </header>
   )
+
 }
 
 Header.defaultProps ={
