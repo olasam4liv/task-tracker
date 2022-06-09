d26b3e8a2d0dffc721dd44fd852dd4092af286fe
Samuel Olatunji <samuel.olatunji@coronationinsurance.com.ng>
2022-06-10 00:00:09 +0100

    Add Task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {useState} from 'react'
- import Header from './components/Header'
- import Tasks from './components/Tasks'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
 function App() {
   const [tasks, setTasks] = useState(
     [
@@ -39,6 +41,7 @@ const toggleReminder  =(id) =>{
   return (
     <div className="container">
       <Header  />
+      <AddTask/>
       {tasks.length > 0 ?  <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No Task Found"}
     </div>
   );
